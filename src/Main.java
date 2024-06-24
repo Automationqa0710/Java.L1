6922e12f5c73496dab08f004d0ef8afe08568e6a
Neto test .1 <khachikyan_r@mail.ru>
2024-06-24 16:48:58 +0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,8 @@ public class Main {
         System.out.println(name + " was driving at speed " + speed);
 
 
+        //////////////////////////////////////////////////////////
+
         int eaters = 5;
 
         int water = 3000;
@@ -28,13 +30,6 @@ public class Main {
         System.out.println((potatoes / eaters) + " картофелин(а/ы)");
         System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
         System.out.println((spices / eaters) + " ложек(ки/ка) специй");
-
-
-
-
-
-
-
 
 
     }
